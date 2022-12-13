5e0c1d5e51997176c5930c6b88ecc8671fd27b40
Felipe Garcia <felipegogarcia@gmail.com>
2022-12-13 14:00:42 -0300

    feat: create app theme components

---
+++ lib/main/ui/components/app_theme.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+ThemeData makeAppTheme() {
+  const primaryColor = Color.fromRGBO(128, 173, 160, 1);
+  const primaryColorDark = Color.fromARGB(255, 9, 61, 41);
+  const primaryColorLight = Color.fromRGBO(138, 234, 146, 1);
+  const secondaryColorDark = Color.fromARGB(255, 42, 52, 109);
+  const textTheme = TextTheme(
+      headline1: TextStyle(
+          fontSize: 30, fontWeight: FontWeight.bold, color: primaryColorDark));
+  const inputDecorationTheme = InputDecorationTheme(
+      enabledBorder: UnderlineInputBorder(
+          borderSide: BorderSide(color: primaryColorLight)),
+      focusedBorder:
+          UnderlineInputBorder(borderSide: BorderSide(color: primaryColor)),
+      alignLabelWithHint: true);
+  final buttonTheme = ButtonThemeData(
+      colorScheme: const ColorScheme.light(primary: primaryColor),
+      buttonColor: primaryColor,
+      splashColor: primaryColorLight,
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      textTheme: ButtonTextTheme.primary,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)));
+
+  return ThemeData(
+      primaryColor: primaryColor,
+      primaryColorDark: primaryColorDark,
+      primaryColorLight: primaryColorLight,
+      secondaryHeaderColor: secondaryColorDark,
+      colorScheme: ColorScheme.fromSwatch().copyWith(
+        primary: primaryColor,
+        secondary: secondaryColorDark,
+      ),
+      backgroundColor: Colors.white,
+      textTheme: textTheme,
+      inputDecorationTheme: inputDecorationTheme,
+      buttonTheme: buttonTheme);
+}
